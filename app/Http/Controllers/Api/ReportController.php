4719e49817b5be65f715467c513d8c538cc3eab7
JuanMa12 <jumaru-97@hotmail.com>
2017-11-24 11:48:41 -0500

    add controller report 2

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -23,18 +23,19 @@ class ReportController extends Controller
       $participants_41_50 = Participant::where('years','>','40')->where('years','<','51')->count();
       $participants_51_100 = Participant::where('years','>','50')->where('years','<','100')->count();
 
-      return response()
-          ->json([
-              'users' => $total_uses,
-              'participants' => $total_participants,
-              'woman' => $participants_woman,
-              'men' => $participants_men,
-              'participants_1_10' => $participants_1_10,
-              'participants_11_20' => $participants_11_20,
-              'participants_21_30' => $participants_21_30,
-              'participants_31_40' => $participants_31_40,
-              'participants_41_50' => $participants_41_50,
-              'participants_51_100' => $participants_51_100,
-          ]);
+      $list_reports = [
+        'users' => $total_uses,
+        'participants' => $total_participants,
+        'woman' => $participants_woman,
+        'men' => $participants_men,
+        'participants_1_10' => $participants_1_10,
+        'participants_11_20' => $participants_11_20,
+        'participants_21_30' => $participants_21_30,
+        'participants_31_40' => $participants_31_40,
+        'participants_41_50' => $participants_41_50,
+        'participants_51_100' => $participants_51_100,
+      ];
+      
+      return response() ->json([ 'list' => $list_reports ]);
   }
 }
