c7ec794e182d76145645ee61e0a81412ff368da5
JuanMa12 <jumaru-97@hotmail.com>
2017-11-27 10:54:45 -0500

    concatenar nombre

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -28,19 +28,23 @@ class Participant extends Model
 
     ];
 
-    protected $appends = ['years'];
+    protected $appends = ['name','years'];
 
     //relations
 
     public function getYearsAttribute()
     {
-
         $born = $this->attributes['born'];
         $hoy = Carbon\Carbon::now();
 
         $date_new = $hoy->format('Y-m-d');
         $year = $date_new-$born;
         return $year;
+    }
 
+    public function getNameAttribute()
+    {
+        $name = $this->attributes['name_one'].' '.$this->attributes['name_two'].' '.$this->attributes['lastname_one'].' '.$this->attributes['lastname_two'];
+        return $name;
     }
 }
