5a2c0363bb853b4d2e6789433a89c17d5e273932
JuanMa12 <jumaru-97@hotmail.com>
2017-11-27 10:06:47 -0500

    edit report controller

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -16,12 +16,43 @@ class ReportController extends Controller
       $participants_woman = Participant::where('gender','femenino')->count();
       $participants_men = Participant::where('gender','masculino')->count();
 
-      $participants_1_10 = Participant::where('years','>','0')->where('years','<','11')->count();
-      $participants_11_20 = Participant::where('years','>','10')->where('years','<','21')->count();
-      $participants_21_30 = Participant::where('years','>','20')->where('years','<','31')->count();
-      $participants_31_40 = Participant::where('years','>','30')->where('years','<','41')->count();
-      $participants_41_50 = Participant::where('years','>','40')->where('years','<','51')->count();
-      $participants_51_100 = Participant::where('years','>','50')->where('years','<','100')->count();
+      $particpants = Participant::all();
+
+      $part_1_10 = [];
+      $part_11_20 = [];
+      $part_21_30 = [];
+      $part_31_40 = [];
+      $part_41_50 = [];
+      $part_51_100 = [];
+
+      foreach ($particpants as $key => $value) {
+        if($value->years > 0 && $value->years < 11){
+          $part_1_10[] = $value->years;
+        }
+        if($value->years > 10 && $value->years < 21){
+          $part_11_20[] = $value->years;
+        }
+        if($value->years > 20 && $value->years < 31){
+          $part_21_30[] = $value->years;
+        }
+        if($value->years > 30 && $value->years < 41){
+          $part_31_40[] = $value->years;
+        }
+        if($value->years > 40 && $value->years < 51){
+          $part_41_50[] = $value->years;
+        }
+        if($value->years > 50 && $value->years < 100){
+          $part_51_100[] = $value->years;
+        }
+      }
+
+
+      $participants_1_10 = count($part_1_10);
+      $participants_11_20 =  count($part_11_20);
+      $participants_21_30 =  count($part_21_30);
+      $participants_31_40 =  count($part_31_40);
+      $participants_41_50 =  count($part_41_50);
+      $participants_51_100 =  count($part_51_100);
 
       $row = [
         'users' => $total_uses,
@@ -37,7 +68,7 @@ class ReportController extends Controller
       ];
 
       $list_reports[] = $row;
-      
+
       return response() ->json([ 'list' => $list_reports ]);
   }
 }
